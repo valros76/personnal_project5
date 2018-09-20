3bd9c103996e6d80b05dc73f8c693b77ee103a2f
valros76 <valerian.dufrene@gmail.com>
2018-09-20 23:02:01 +0200

    Add initial home view and route configurate for home()

--- index.php
+++ index.php
@@ -1,10 +1,21 @@
 <?php
-
+session_start();
 function loadClass($class){
     require('models/classes/'.$class.'.php');
 }
 spl_autoload_register('loadClass');
 
+require('controllers/appController.php');
 
+if(isset($_GET['action'])){
+
+    if($_GET['action'] === 'home'){
+        home();
+    }
+
+}
+else{
+    home();
+}
 
 ?>
---
+++ views/homeView.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+</head>
+<body>
+    <header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a class="navbar-brand" href="?action=home">Home</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarColor01">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="?action=articles">Articles <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="?action=portfolio">Portfolio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="?action=inscription">Inscription</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="?action=connexion">Connexion</a>
+                </li>
+            </ul>
+            <form action="?action=submitSearch" method="post" class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Rechercher...">
+            <button class="btn btn-secondary my-2 my-sm-0" type="submit">Rechercher</button>
+            </form>
+        </div>
+        </nav>
+    </header>
+</body>
+</html>
