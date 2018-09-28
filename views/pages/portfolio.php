e343c4c4dc85f0a7038266c77987f99836d196a7
valros76 <valerian.dufrene@gmail.com>
2018-09-28 21:06:37 +0200

    Remove importation of rating.js and cookie.js

--- views/pages/portfolio.php
+++ views/pages/portfolio.php
@@ -101,8 +101,6 @@
 $content = ob_get_clean();?>
 
 <?php ob_start();
-    echo '<script src="public/js/cookies.js"></script>';
-    echo '<script src="public/js/rating.js"></script>';
     echo '<script src="public/js/github.js"></script>';
 $scripts = ob_get_clean(); ?>
 
